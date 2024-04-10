56a57ee70948cd5f9fb9642ae20b2c9201602ffb
VishnurajKarockal <vishnurajkarockal@gmail.com>
2024-04-10 19:15:05 +0530

    Cointab assignment first commit includes both backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const app = express();
 const cors = require("cors");
 const { postRouter } = require("./Routes/post.routes");
 app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Yeah your cointab backend is working fine!");
+});
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 const port = process.env.port;
